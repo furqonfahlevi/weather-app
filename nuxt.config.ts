efdcca484fa030f143a9ec841aeb7a51da6128e5
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-10-26 22:48:43 +0700

    add axios and weather api setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss"], ['@nuxtjs/dotenv'], ['@nuxtjs/axios'],
   css: ["~/assets/css/tailwind.css"],
 });
