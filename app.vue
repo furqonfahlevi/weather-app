073936a3d5e1f8cf743c1f20328f5f9d4eb29bf1
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-10-28 01:59:01 +0700

    add contentcontainer component

--- app.vue
+++ app.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="w-full mx-auto flex flex-col justify-center items-center p-10">
     <AppHeader />
-    <div class="flex w-full mt-20 flex-col sm:flex-row gap-8">
-      <div class="w-full px-2">
-        <CurrentWeather :weatherData="weatherData" />
-        <AirConditions :weatherData="weatherData" />
-      </div>
-    </div>
+    <ContentContainer :weatherData="weatherData" />
   </div>
 </template>
 <script lang="ts">
-import AppHeader from "./components/AppHeader.vue";
-import CurrentWeather from "./components/CurrentWeather.vue";
-import AirConditions from "./components/AirConditions.vue";
 import { useWeather } from "./composable/useWeather";
 
+import AppHeader from "./components/AppHeader.vue";
+import ContentContainer from "./components/ContentContainer.vue";
+
 export default {
-  components: { AppHeader, CurrentWeather, AirConditions },
+  components: { AppHeader, ContentContainer },
   name: "App",
   setup() {
     const { weatherData } = useWeather();
---
+++ components/ContentContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex w-full mt-20 flex-col sm:flex-row gap-8">
+    <div class="w-full px-2">
+      <CurrentWeather :weatherData="weatherData" />
+      <AirConditions :weatherData="weatherData" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import CurrentWeather from "./CurrentWeather.vue";
+import AirConditions from "./AirConditions.vue";
+
+export default {
+  name: "ContentContainer",
+  components: { CurrentWeather, AirConditions },
+  props: {
+    weatherData: Object,
+  },
+};
+</script>
